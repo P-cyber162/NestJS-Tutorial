1bb15d38e9a1999048e10c371d5e3048a6ab8de4
P-cyber162 <anakwaprince204@gmail.com>
2026-09-10 14:54:58 +0000

    Update Prettier configuration, enhance PropertyController with improved validation, and set global ValidationPipe in PropertyModule

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,34 +1,59 @@
-import { Body, Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreatePropertyDto } from './dto/createProperty.dto.js';
+import { idParamDto } from './dto/idParam.dto.js';
+import { ParseIdPipe } from './pipes/parseIdPipes.js';
 
 @Controller('property')
 export class PropertyController {
-    @Get()
-    findAll() {
-        return "All properties";
-    }
+  @Get()
+  findAll() {
+    return 'All properties';
+  }
 
-    @Get(":id")
-    findOne(@Param("id", ParseIntPipe) id: Number, @Query("sort", ParseBoolPipe) sort: Boolean) {
-        return {
-            id: typeof(id),
-            sort: typeof(sort),
-            date: new Date()
-        };
-    }
+  @Get(':id')
+  findOne(
+    @Param('id', ParseIntPipe) id: Number,
+    @Query('sort', ParseBoolPipe) sort: Boolean,
+  ) {
+    return {
+      id: typeof id,
+      sort: typeof sort,
+      date: new Date(),
+    };
+  }
 
-    @Post()
-    //@UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-    @HttpCode(201)
-    create(@Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, groups:['create'] })) body: CreatePropertyDto) {
-        return {
-            message: "Properties created!",
-            data: body,
-        }
-    }
+  @Post()
+  //@UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  @HttpCode(201)
+  create(
+    @Body()
+    body: CreatePropertyDto,
+  ) {
+    return {
+      message: 'Properties created!',
+      data: body,
+    };
+  }
 
-    @Patch(":id")
-    update(@Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, groups:['update'], always: true })) body: CreatePropertyDto) {
-        return body
-    }
+  @Patch(':id')
+  update(
+    @Param('id', ParseIdPipe) id: number,
+    @Body()
+    body: CreatePropertyDto,
+  ) {
+    return body;
+  }
 }
